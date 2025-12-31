49ffbd229c4dd4ed2f1652a1a26b5ea3cc34dd3a
merabza <merabza@gmail.com>
2025-12-31 15:50:11 +0400

    202512311550

--- ReServer/Program.cs
+++ ReServer/Program.cs
@@ -11,33 +11,16 @@ using System.IO;
 using System.Reflection;
 using WindowsServiceTools;
 
-//using WebInstallers;
-//using AssemblyReference = ApiExceptionHandler.AssemblyReference;
-
 try
 {
     Console.WriteLine("Loading...");
 
     const string appName = "ReServer";
-    //const int versionCount = 1;
 
     var header = $"{appName} {Assembly.GetEntryAssembly()?.GetName().Version}";
     Console.WriteLine(FiggleFonts.Standard.Render(header));
 
     const string appKey = "CF39BBE3-531B-417E-AC20-3605313D0F94";
-
-    ////პროგრამის ატრიბუტების დაყენება 
-    //ProgramAttributes.Instance.AppName = appName;
-    //ProgramAttributes.Instance.AppKey = appKey;
-
-    //var parameters = new Dictionary<string, string>
-    //{
-    //    //{ SignalRMessagesInstaller.SignalRReCounterKey, string.Empty },//Allow SignalRReCounter
-    //    { ConfigurationEncryptInstaller.AppKeyKey, appKey },
-    //    { SwaggerInstaller.AppNameKey, appName },
-    //    { SwaggerInstaller.VersionCountKey, 1.ToString() }
-    //    //{ SwaggerInstaller.UseSwaggerWithJwtBearerKey, string.Empty },//Allow Swagger
-    //};
 
     var builder = WebApplication.CreateBuilder(new WebApplicationOptions
     {
@@ -56,26 +39,8 @@ try
         .AddHostedServices(debugMode).AddHttpClient();
     // @formatter:on
 
-    //if (!builder.InstallServices(debugMode, args, parameters,
-
-    //        //WebSystemTools
-    //        ApiExceptionHandler.AssemblyReference.Assembly,
-    //        ConfigurationEncrypt.AssemblyReference.Assembly,
-    //        HttpClientInstaller.AssemblyReference.Assembly,
-    //        SerilogLogger.AssemblyReference.Assembly,
-    //        SwaggerTools.AssemblyReference.Assembly,
-    //        TestToolsApi.AssemblyReference.Assembly,
-    //        WindowsServiceTools.AssemblyReference.Assembly,
-
-    //        //ReServer
-    //        ReServer.AssemblyReference.Assembly))
-    //    return 2;
-
     // ReSharper disable once using
     using var app = builder.Build();
-
-    //if (!app.UseServices(debugMode))
-    //    return 3;
 
     Log.Information("Directory.GetCurrentDirectory() = {0}", Directory.GetCurrentDirectory());
     Log.Information("AppContext.BaseDirectory = {0}", AppContext.BaseDirectory);
